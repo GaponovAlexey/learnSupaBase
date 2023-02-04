c012c3c708f456bc3a29ce0465c8282f0638fcc9
kekelberi <alexei.gaponov@gmail.com>
2023-02-04 13:48:01 -0600

    save

--- src/app/reg/Login.tsx
+++ src/app/reg/Login.tsx
@@ -10,31 +10,21 @@ export const LoginApp = () => {
   const redirect = useRouter()
   const [dataUser, setUser] = useState({})
 
-
+ 
 
  
-  console.log("user", dataUser)
 
-  const [login, setLogin] = useState("")
-  const [password, setPassword] = useState("")
+  
   return (
     <div>
-      <button>Discord</button>
-      <form action="form">
-        <p>login</p>
-        <input
-          type="login"
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
+      <div>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{theme: ThemeSupa}}
+          theme="dark"
+          providers={["discord", "apple"]}
         />
-        <p>password</p>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </form>
-      <button>logout</button>
+      </div>
     </div>
   )
 }
--- supabase.tsx
+++ supabase.tsx
@@ -2,4 +2,4 @@ import { createClient } from "@supabase/supabase-js"
 
 const SUPABASE_URL = "https://iakjucmhukqakhswuqgh.supabase.co"
 const SUPABASE_KEY = process.env.DB_KEY
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY as string)
+export const supabase = createClient(SUPABASE_URL, String(SUPABASE_KEY))
