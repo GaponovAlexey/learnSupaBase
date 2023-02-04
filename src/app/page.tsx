c9a29f72a622ed46e257baaf61af80270e7767bc
kekelberi <alexei.gaponov@gmail.com>
2023-02-03 20:01:16 -0600

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
+import Link from "next/link"
+import React from "react"
 import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      start
-      <button>Discord</button>
-      <form action="form">
-        <p>login</p>
-        <input type="login" />
-        <p>password</p>
-        <input type="password" />
-      </form>
-      <button>logout</button>
+      <p>home put down</p>
+      <Link href={"/main"}>Login</Link>
     </main>
   )
 }
