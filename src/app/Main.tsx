48f414010c1632ca2d6d85444c761aeb41e9c91c
kekelberi <alexei.gaponov@gmail.com>
2023-02-04 12:34:50 -0600

    save

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,12 +1,23 @@
+"use client"; // this is a client component
+
 import Link from "next/link"
-import React from "react"
+import { useState } from "react"
 import styles from "./page.module.css"
+
+
+
+
 export const Main = () => {
+  const [isLogin, setIsLogin] = useState(true)
   return (
     <div>
       <main className={styles.main}>
+        {isLogin ? (
+          <Link href={"/registration/login"}>Login</Link>
+        ) : (
+          <Link href={"/registration/LogOut"}>LogOut</Link>
+        )}
         <p>home put down</p>
-        <Link href={"/main"}>Login</Link>
       </main>
     </div>
   )
--- src/app/reg/Login.tsx
+++ src/app/reg/Login.tsx
@@ -1,6 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react"
+import { useRouter } from "next/router"
+import { supabase } from "supabase"
+
+
 
 export const LoginApp = () => {
-  const [da, setDa] = useState(0)
-  return <div>Logins{da}</div>
+  const redirect = useRouter()
+  const [dataUser, setUser] = useState({})
+
+
+
+ 
+  console.log("user", dataUser)
+
+  const [login, setLogin] = useState("")
+  const [password, setPassword] = useState("")
+  return (
+    <div>
+      <button>Discord</button>
+      <form action="form">
+        <p>login</p>
+        <input
+          type="login"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <p>password</p>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+      <button>logout</button>
+    </div>
+  )
 }
